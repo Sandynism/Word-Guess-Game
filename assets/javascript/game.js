611ea85123121f800d03b55362efb3a4a20d6235
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-06-22 21:55:47 -0400

    added func

---
+++ assets/javascript/game.js
@@ -0,0 +1,68 @@
+let gamePlay = false;
+// SETTINGS
+let $word = document.getElementById('placeholders')
+let $lettersGuessed = document.getElementById('lettersGuessed')
+let $livesCount = document.getElementById('livesCount')
+
+let $wins = document.getElementById('wins')
+$wins.innerText = 0
+let $losses = document.getElementById('losses')
+$losses.innerText = 0
+// let foodBank = ["DOLE WHIP", "DIPPING DOTS", "CORN DOG", "FRIED TWINKIES", "CHEESE FRIES", "SLUSHIE", "CHICKEN TENDERS"]
+let foodBank = ["cheese", "apple", "orange", "bi"]
+let currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
+
+let letters = []
+let $newGameButton = document.getElementById('newGameButton')
+
+
+$newGameButton.addEventListener('click', () => {
+    letters = [];
+    currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
+
+    $livesCount.innerText = 8
+    $word.innerText = hideString(currentFoodItem, letters)
+    $lettersGuessed.innerText = ""
+    
+})
+
+function hideString(currentFoodItem, array) {
+    let finalWord = ""
+    for (let i = 0; i < currentFoodItem.length; i++) {
+        if (array.includes(currentFoodItem[i])) {
+            finalWord += currentFoodItem[i]
+        } else {
+            finalWord += "#"
+        }
+    }
+    return finalWord
+}
+
+function checkUserInput(input) {
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+    let lowerInput = input.toLowerCase()
+    
+    if (alphabet.includes(lowerInput) && !letters.includes(lowerInput)) {
+      letters.push(input)
+    } 
+}
+
+
+document.addEventListener("keyup", (event) => {
+    checkUserInput(event.key)
+    $word.innerText = hideString(currentFoodItem, letters)
+
+    $lettersGuessed.innerText = letters.join(", ")
+
+    $livesCount.innerHTML -= 1
+
+    $wins.innerText = parseInt($wins.innerText) + 1
+
+    $losses.innerText = parseInt($losses.innerText) + 1 
+
+})
+
+function endGame()
+
+
+
