82186bcc3c4e7825c87b974c1c9c0dcb7d5eff5b
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-06-25 12:59:21 -0400

    changes

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,4 +1,3 @@
-let gamePlay = false;
 // SETTINGS
 let $word = document.getElementById('placeholders')
 let $lettersGuessed = document.getElementById('lettersGuessed')
@@ -9,13 +8,14 @@ $wins.innerText = 0
 let $losses = document.getElementById('losses')
 $losses.innerText = 0
 
-let foodBank = ["dole whip", "dipping dots", "corn dog", "fried oreos", "cheese fries", "slushie", "chicken tenders", "churros", "turkey leg", "candy apple", "beignets", "mac n cheese"]
+let foodBank = ["cheese"]
+// let foodBank = ["dole whip", "dipping dots", "corn dog", "fried oreos", "cheese fries", "slushie", "chicken tenders", "churros", "turkey leg", "candy apple", "beignets", "mac n cheese"]
 let currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
 
 let letters = []
 let $newGameButton = document.getElementById('newGameButton')
 
-
+// new game
 $newGameButton.addEventListener('click', () => {
     letters = [];
     currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
@@ -23,14 +23,17 @@ $newGameButton.addEventListener('click', () => {
     $livesCount.innerText = 8
     $word.innerText = hideString(currentFoodItem, letters)
     $lettersGuessed.innerText = ""
-    
+
 })
 
-function hideString(currentFoodItem, array) {
+
+function hideString(foodString, array) {
     let finalWord = ""
-    for (let i = 0; i < currentFoodItem.length; i++) {
-        if (array.includes(currentFoodItem[i])) {
-            finalWord += currentFoodItem[i]
+    for (let i = 0; i < foodString.length; i++) {
+        if (array.includes(foodString[i])) {
+            finalWord += foodString[i]
+        } else if (foodString[i] === " ") {
+            finalWord += " "
         } else {
             finalWord += "#"
         }
@@ -41,28 +44,58 @@ function hideString(currentFoodItem, array) {
 function checkUserInput(input) {
     let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
     let lowerInput = input.toLowerCase()
-    
-    if (alphabet.includes(lowerInput) && !letters.includes(lowerInput)) {
-      letters.push(input)
-    } 
+    let validLetter = alphabet.includes(lowerInput)
+
+    if (validLetter && !letters.includes(lowerInput)) {
+        letters.push(lowerInput)
+    } else if (validLetter && letters.includes(lowerInput)) {
+        alert('Letter already guessed')
+    }
+
+    if (validLetter && !currentFoodItem.toLowerCase().includes(lowerInput)) {
+        $livesCount.innerHTML -= 1
+    }
+}
+
+function checkWinner(string, array) {
+    for (let i = 0; i < string.length; i++) {
+        if (!array.includes(string[i])) {
+            return false
+        }
+    }
+    return true
+}
+
+function checkGameStatus() {
+    if ($livesCount.innerHTML === "0") {
+        $losses.innerText = parseInt($losses.innerText) + 1//change to reset game
+    }
+
+    if (checkWinner(currentFoodItem, letters)) {
+        $wins.innerText = parseInt($wins.innerText) + 1
+    }
 }
 
 
 document.addEventListener("keyup", (event) => {
+
     checkUserInput(event.key)
+    checkGameStatus()
     $word.innerText = hideString(currentFoodItem, letters)
 
     $lettersGuessed.innerText = letters.join(", ")
 
-    $livesCount.innerHTML -= 1
 
-    $wins.innerText = parseInt($wins.innerText) + 1
 
-    $losses.innerText = parseInt($losses.innerText) + 1 
+
+
+
 
 })
 
-function endGame()
+// function endGame()
 
-
+//add thing that shows spaces as empty space and not #
+//restart new game if player won or if lives are at 0
+//
 
