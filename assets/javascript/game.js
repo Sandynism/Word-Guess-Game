faa175e306436eb64e262afeab06edadcf0c5562
sandynism <31261960+Sandynism@users.noreply.github.com>
2018-06-25 18:32:24 -0400

    styles

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -8,34 +8,71 @@ $wins.innerText = 0
 let $losses = document.getElementById('losses')
 $losses.innerText = 0
 
-let foodBank = ["dole whip"]
-// let foodBank = ["dole whip", "dipping dots", "corn dog", "fried oreos", "cheese fries", "slushie", "chicken tenders", "churros", "turkey leg", "candy apple", "beignets", "mac n cheese"]
+// let foodBank = ["dole whip"]
+let foodBank = ["dole whip", "dipping dots", "corn dog", "fried oreos", "cheese fries", "slushie", "chicken tenders", "churros", "turkey leg", "candy apple", "beignets", "mac n cheese"]
 let currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
 
 let letters = []
 let $newGameButton = document.getElementById('newGameButton')
+let $gamePage = document.getElementById('gamePage')
+let $introPage = document.getElementById('introPage')
+let $resetButton = document.getElementById('resetButton')
+
+// function gameStart() {
+//     if ($newGameButton.clicked === true) {
+//         $introPage.style.display = "none";
+//         $gamePage.style.display = "block"; 
+//     } else {
+//         $gamePage.style.display = "none";
+//     }
+// }
+
+// $newGameButton.onclick = function() {
+//     $gamePage.style.display = "block";
+// }
+$(document).ready(function () {
+    $("#newGameButton").click(function () {
+        $(".introPage").addClass("disappear")
+        $(".gamePage").removeClass("disappear")
+    })
+})
 
 // new game
-$newGameButton.addEventListener('click', () => {
+function startGame() {
     letters = [];
     currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
 
     $livesCount.innerText = 8
     $word.innerText = hideString(currentFoodItem, letters)
     $lettersGuessed.innerText = ""
+}
 
+$newGameButton.addEventListener('click', () => {
+    // letters = [];
+    // currentFoodItem = foodBank[Math.floor(Math.random() * foodBank.length)];
+
+    // $livesCount.innerText = 8
+    // $word.innerText = hideString(currentFoodItem, letters)
+    // $lettersGuessed.innerText = ""
+    startGame()
 })
 
-//
+$resetButton.addEventListener('click', () => {
+    $wins.innerText= 0
+    $losses.innerText= 0
+    $livesCount.innerText = 8
+    startGame()
+})
+
 function hideString(foodString, array) {
     let finalWord = ""
     for (let i = 0; i < foodString.length; i++) {
         if (array.includes(foodString[i])) {
             finalWord += foodString[i]
         } else if (foodString[i] === " ") {
-            finalWord += " "
+            finalWord += "  "
         } else {
-            finalWord += "#"
+            finalWord += "_"
         }
     }
     return finalWord
@@ -76,20 +113,13 @@ function checkGameStatus() {
     }
 }
 
-
 document.addEventListener("keyup", (event) => {
 
     checkUserInput(event.key)
     checkGameStatus()
     $word.innerText = hideString(currentFoodItem, letters)
 
-    $lettersGuessed.innerText = letters.join(", ")
-
-
-
-
-
-
+    // $lettersGuessed.innerText = letters.join(", ")
 
 })
 
